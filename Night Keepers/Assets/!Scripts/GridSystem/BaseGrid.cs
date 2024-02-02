a7b6334ddad99bc0a8572717075ea912105185a9
Muhammed Atmaca <rmk.atmaca@gmail.com>
2024-02-02 16:15:35 +0300

    Folders and name changes

--- Night Keepers/Assets/!Scripts/GridSystem/BaseGrid.cs
+++ Night Keepers/Assets/!Scripts/GridSystem/BaseGrid.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public class BaseGrid : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
