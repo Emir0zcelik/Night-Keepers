0f3008d03d93305f5b17e2761c6f3a2a4a68b911
Muhammed Atmaca <rmk.atmaca@gmail.com>
2024-05-29 11:29:32 +0300

    Main Menu UI

--- Night Keepers/Assets/!Scripts/MainMenu.cs
+++ Night Keepers/Assets/!Scripts/MainMenu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,12 +5,11 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("SampleScene");
+        SceneManager.LoadSceneAsync("GameScene");
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-
 }
