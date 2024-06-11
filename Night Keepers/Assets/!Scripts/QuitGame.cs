bf26428ac834e7319c7550847cb7497422f6c6f8
Muhammed Atmaca <rmk.atmaca@gmail.com>
2024-06-11 20:40:17 +0300

    quit game button

---
+++ Night Keepers/Assets/!Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace NightKeepers
+{
+    public class QuitGame : MonoBehaviour
+    {
+        public void QuitGameFromUI()
+        {
+            Application.Quit();
+        }
+    }
+}
